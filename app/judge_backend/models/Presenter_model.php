fb8491fb43ba451f2f152d2b0b22cbf63e507109
Mark Adkins <mark.adkins@sbcs.com>
2017-03-02 22:55:36 -0500

    started on adding functionality to backend for registration

--- app/judge_backend/models/Presenter_model.php
+++ app/judge_backend/models/Presenter_model.php
@@ -6,5 +6,26 @@ class Presenter_model extends CI_Model {
                 parent::__construct();
         }
 
+        public function get_all_presenters() {
+                $query = $this->db->select('*')
+                                ->from('presenter')
+                                ->order_by('presenter_name', 'ASC')
+                                ->get();
+
+                $result = $query->result_array();
+
+                return $result;
+        }
+
+        public function get_presenter($presenter_id) {
+                $query = $this->db->select('*')
+                                ->from('presenter')
+                                ->where('presenter_id =' . $presenter_id)
+                                ->get();
+                $result = $query->result_array();
+
+                return $result;
+        }
+
 }
 ?>
