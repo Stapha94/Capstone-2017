69f4f5c40decc7bb826dba51c3d164fb9aabcdce
Mark Adkins <mark.adkins@sbcs.com>
2017-03-02 20:46:40 -0500

    set up Gabe's login and dashboard pages to load via angular

---
+++ app/JUDGE/src/admin-dashboard/admin-dashboard-controller.js
@@ -0,0 +1,5 @@
+app.controller('adminDashboardController', ['$scope', 
+    function($scope) {
+        // dashboardy stuff wilst goeth here
+    }
+])
--- app/JUDGE/src/app-states.js
+++ app/JUDGE/src/app-states.js
@@ -6,6 +6,27 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
             controller: 'landingController',
             controllerAs: 'ctrl'
         })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'src/admin-login/admin-login.html',
+            controller: 'adminLoginController',
+            controllerAs: 'ctrl'
+        })
+        .state('admin', {
+            url: '/admin',
+            abstract: true,
+            templateUrl: 'src/admin-dashboard/admin-nav.html'
+        })
+        .state('admin.dashboard', {
+            url: '/dashboard',
+            views: {
+                'admin': {
+                    templateUrl: 'src/admin-dashboard/admin-dashboard.html',
+                    controller: 'adminDashboardController',
+                    controllerAs: 'ctrl'
+                }
+            }
+        })
     
     // default to landing page
     $urlRouterProvider.otherwise('/home');
