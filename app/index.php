435382e911212d8b1317e5d02b22677b1c0cb0dc
Mark Adkins <mark.adkins@sbcs.com>
2017-03-04 14:33:42 -0500

    FINALLY fixed cors error in chrome. the two ends are friends again

--- app/index.php
+++ app/index.php
@@ -1,12 +1,19 @@
 <?php
-// Allow from any origin
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header('Access-Control-Max-Age: 86400');    // cache for 1 day
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') 
-{
-	http_response_code(200);
-}
+// Allow from any origin. Answer from stackoverflow: http://stackoverflow.com/questions/10883211/deadly-cors-when-http-localhost-is-the-origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');    
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+}   
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+} 
 /**
  * CodeIgniter
  *
