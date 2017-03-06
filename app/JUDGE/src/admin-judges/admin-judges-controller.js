7df793e7f3da456c876f44f409f1e5cd84f52245
Gabehoverman <Hoverman@Marshall.edu>
2017-03-06 18:55:16 -0500

    Small tweaks and edits.

--- app/JUDGE/src/admin-judges/admin-judges-controller.js
+++ app/JUDGE/src/admin-judges/admin-judges-controller.js
@@ -4,7 +4,7 @@ app.controller('adminJudgesController', ['$scope',
         var one = {id:1,name:"Bob",score:100,project:"Common Cold",category:"Marshall",active: true};
         var two = {id:2,name:"Steve",score:95, project:"Symptom Relief",category:"JCE",active: false};
         var three = {id:3,name:"James",score:85,project:"Research Topic",category:"Marshall",active: true};
-        var four = {id:3,name:"Mark",score:85,project:"Soda and Health",category:"JCE",active: true};
+        var four = {id:4,name:"Mark",score:85,project:"Soda and Health",category:"JCE",active: true};
 
         var data = [one,two,three,four];
 
