6ac6e576a82dd1f544b9855cf90088513b634b22
Mark Adkins <mark.adkins@sbcs.com>
2017-04-03 16:51:36 -0400

    godaddy settings. DO NOT ALTER IN THIS BRANCH

--- JUDGE/src/app.js
+++ JUDGE/src/app.js
@@ -2,8 +2,8 @@
 app = angular.module('app', ['ui.router', 'ui.bootstrap', 'angular-loading-bar'])
     .constant('CONFIG', {
         // This is the url for the database. Change it between local and host when needed.
-        DBURL: 'http://localhost:8800/'
-      //DBURL: 'https://qss-judge.heroku.com/'
+        //DBURL: 'http://localhost:8800/'
+      DBURL: 'http://judge.marshallcite.org/'
     })
     .run(($log, $rootScope, routeInterceptor) => {
         // Set listener on state changes
--- JUDGE_backend/config/database.php
+++ JUDGE_backend/config/database.php
@@ -71,7 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'local';
+$active_group = 'godaddy';
 $query_builder = TRUE;
 
 $db['local'] = array(
@@ -95,14 +95,14 @@ $db['local'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-/**
-$db['heroku'] = array(
+
+$db['godaddy'] = array(
 	'dsn'	=> '',
-	'hostname' => $heroku_server['host'],
-	'username' => $heroku_server['username'],
-	'password' => $heroku_server['password'],
-	'database' => $heroku_server['db'],
-	'dbdriver' => 'postgre',
+	'hostname' => $godaddy_server['host'],
+	'username' => $godaddy_server['username'],
+	'password' => $godaddy_server['password'],
+	'database' => $godaddy_server['db'],
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -117,4 +117,3 @@ $db['heroku'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
- */
