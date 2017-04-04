1e2e970442452995aacd97107ec1e82114e35778
Mark Adkins <mark.adkins@sbcs.com>
2017-04-04 00:36:40 -0400

    fixed settings

--- JUDGE/src/app.js
+++ JUDGE/src/app.js
@@ -3,7 +3,7 @@ app = angular.module('app', ['ui.router', 'ui.bootstrap', 'angular-loading-bar']
     .constant('CONFIG', {
         // This is the url for the database. Change it between local and host when needed.
         //DBURL: 'http://localhost:8800/'
-      DBURL: 'http://judge.marshallcite.org/'
+      DBURL: 'http://judge.marshallcite.org/app/'
     })
     .run(($log, $rootScope, routeInterceptor) => {
         // Set listener on state changes
