e029dccd7c1f006e0f21ef0e5be9e1115ee075b4
Mark Adkins <mark.adkins@sbcs.com>
2017-04-13 10:11:56 -0400

    added pdfmake

--- JUDGE/src/pages/judge/judge-dashboard/judge-dashboard-controller.js
+++ JUDGE/src/pages/judge/judge-dashboard/judge-dashboard-controller.js
@@ -34,6 +34,36 @@ class JudgeDashboardController {
         };
     }
 
+    downloadAbstract(poster) {
+        var name = poster.posterTitle+'.pdf';
+        var abstractDoc = {
+            header: function(currentPage, pageCount) { 
+                return { text: currentPage.toString() + ' of ' + pageCount, alignment: 'right', margin: [2, 2, 5, 0]}; 
+            },
+            background: { text: poster.posterTitle, bold: true, margin: [5, 2, 2, 0]},
+            content: [
+                { text: poster.posterTitle, style: 'header'},
+                { text: 'Objective', style: 'header'},
+                poster.objective,
+                { text: 'Methods', style: 'header'},
+                poster.methods,
+                { text: 'Results', style: 'header'},
+                poster.results,
+                { text: 'Conclusion', style: 'header'},
+                poster.conclusion
+            ],
+
+            styles: {
+                header: {
+                    fontSize: 22,
+                    bold: true,
+                    margin: [5, 10]
+                }
+            }
+        };
+        pdfMake.createPdf(abstractDoc).download(name);
+    }
+
 }
 
 JudgeDashboardController.$inject = ['$scope', '$state', '$stateParams', 'forms'];
