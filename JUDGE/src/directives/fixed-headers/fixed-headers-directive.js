ac12fe726fc43ddd4ee0e7cd731de4df17eef4ed
Mark Adkins <mark.adkins@sbcs.com>
2017-04-22 14:52:24 -0400

    still working on fixed headers

--- JUDGE/src/directives/fixed-headers/fixed-headers-directive.js
+++ JUDGE/src/directives/fixed-headers/fixed-headers-directive.js
@@ -1,42 +1,40 @@
 class FixedHeadersDirective {
 
-    constructor($timeout) {
+    constructor($timeout, $window) {
         this.restrict = 'A';
+        this.controller = 'fixedHeadersController';
         this.$timeout = $timeout;
+        this.$window = angular.element($window);
     }
 
     link(scope, element, attribute, controller) {
         this.$timeout(() => {
-            var tbodies = element.find('tbody');
-            if(tbodies.length > 0) {
-                var tbody = tbodies[0];
-                if(tbody.offsetHeight > 300) {
-                    tbody.style.height = '300px';
-                }
+            if(element[0].offsetHeight > 300) {
+                element[0].style.height = '300px';
             }
             var rows = element.find('tr');
             if(rows.length > 1) {
                 var headers = rows[0].children; // Headers are the first row.
                 var body = rows[1].children; // Use the first row for the body
                 var index = 0;
-                _.forEach(headers, (header) => {
+/*                _.forEach(headers, (header) => {
                     if(header.offsetWidth < body[index].offsetWidth) {
                         header.style.width = body[index].offsetWidth+'px';
                     } else {
                         body[index].style.width = header.offsetWidth+'px';
                     }
                     index++;
-                })
+                })*/
             }
         }, 60);
     }
 
-    static directiveFactory($timeout) {
-        FixedHeadersDirective.instance = new FixedHeadersDirective($timeout);
+    static directiveFactory($timeout, $window) {
+        FixedHeadersDirective.instance = new FixedHeadersDirective($timeout, $window);
         return FixedHeadersDirective.instance;
     }
 }
 
 FixedHeadersDirective.directiveName = 'fixedHeaders';
-FixedHeadersDirective.$inject = ['$timeout'];
+FixedHeadersDirective.$inject = ['$timeout', '$window'];
 app.directive(FixedHeadersDirective.directiveName, FixedHeadersDirective.directiveFactory);
